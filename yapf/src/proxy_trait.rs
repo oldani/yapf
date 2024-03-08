e895ed3c649668e8aa4458c8558b473aaa1ef9d4
Ordanis Sanchez <ordanisanchez@gmail.com>
2024-03-08 19:23:08 -0400

    yapf: Change proxy trait

--- yapf/src/proxy_trait.rs
+++ yapf/src/proxy_trait.rs
@@ -1,8 +1,12 @@
-use http::{request::Parts, Response, Uri};
+use async_trait::async_trait;
+use hyper::body::Body;
+use hyper::{http::request::Parts, Response, Uri};
 
-type RequestHeaders = Parts;
-type ResponseHeaders = Parts;
-pub trait HttpProxy {
+pub type RequestHeaders = Parts;
+pub type ResponseHeaders = Parts;
+
+#[async_trait]
+pub trait Proxy {
     /// The per request object to share state across the different filters
     type CTX;
 
@@ -15,39 +19,28 @@ pub trait HttpProxy {
     /// return a response for this request.
     async fn request_filter(
         &self,
-        _request: RequestHeaders,
+        _request: &RequestHeaders,
         _ctx: &mut Self::CTX,
-    ) -> Result<(), Response<()>> {
+    ) -> Result<(), Response<Body>> {
         Ok(())
     }
     /// Define where the proxy should sent the request to.
     ///
     /// The returned [Uri] contains the information regarding where this request should be forwarded to.
-    async fn upstream_addr<T>(&self, request: RequestHeaders, ctx: &mut Self::CTX) -> Uri;
+    async fn upstream_addr(&self, request: &RequestHeaders, ctx: &mut Self::CTX) -> Option<Uri>;
 
-    // async fn request_body_filter<T>(
-    //     &self,
-    //     _request: Request<T>,
-    //     _ctx: &mut Self::CTX,
-    // ) -> Result<(), Response<()>> {
-    //     Ok(())
-    // }
+    /// Modify the request header before it is send to the upstream
+    ///
+    /// This is the last chance to modify the request before it is sent to the upstream.
+    async fn upstream_request_filter(&self, _request: &mut RequestHeaders, _ctx: &mut Self::CTX) {}
 
     /// Modify the response header before it is send to the downstream
     ///
-    async fn response_filter<T>(
+    async fn response_filter(
         &self,
         _upstream_response: ResponseHeaders,
         _ctx: &mut Self::CTX,
-    ) -> Result<(), Response<()>> {
+    ) -> Result<(), Response<Body>> {
         Ok(())
     }
-
-    // async fn response_body_filter<T>(
-    //     &self,
-    //     _response: Response<T>,
-    //     _ctx: &mut Self::CTX,
-    // ) -> Result<(), Response<()>> {
-    //     Ok(())
-    // }
 }
