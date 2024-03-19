0fdcf2a667fdaf587566a322c0285020247427f8
Ordanis Sanchez <ordanisanchez@gmail.com>
2024-03-19 08:43:18 -0400

    [yapf]: Add ListenFds

--- yapf/src/services.rs
+++ yapf/src/services.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+use std::os::unix::io::RawFd;
+use std::sync::{Arc, Mutex};
+
 use async_trait::async_trait;
 use tokio::sync::watch;
 
@@ -6,3 +10,10 @@ use tokio::sync::watch;
 pub trait BackgroundService {
     async fn start(&self, shutdown: watch::Receiver<bool>);
 }
+
+/// Container for open file descriptors and their associated bind addresses.
+pub struct Fds {
+    map: HashMap<String, RawFd>,
+}
+
+pub type ListenFds = Arc<Mutex<Fds>>;
