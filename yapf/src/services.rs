79884fa46b6ef38af73a92e2c82d92b346eb41b6
Ordanis Sanchez <ordanisanchez@gmail.com>
2024-05-01 10:27:09 -0400

    [yapf]: Remove duplicate code

--- yapf/src/services.rs
+++ yapf/src/services.rs
@@ -1,13 +1,1 @@
-use std::collections::HashMap;
-use std::os::unix::io::RawFd;
-use std::sync::{Arc, Mutex};
 
-use async_trait::async_trait;
-use pingora_server::server::{ListenFds, ShutdownWatch};
-use tokio::sync::watch;
-
-#[cfg(feature = "pingora")]
-#[async_trait]
-pub trait BackgroundService {
-    async fn start(&self, shutdown: ShutdownWatch);
-}
